a3b0e538acb5843c287d0e5d8282da5190be2063
NagarjunaKadari1008 <nagarjuna@veefin.com>
2024-01-17 22:45:37 +0530

    first

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 print('hello world')
 print("This is my comment")
-print("I am Nagarjuna")
-print('hello mumbai')
-print('mubai shaar')
-print("I am Nagarjuna")
-print("sjjsjss")
-print("gahsjjskskks")
+
 
